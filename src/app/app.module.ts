2e9a99338c07d9929b4652e4b6a46b2c64bd5075
Oumezzine Farah <oumezzine.farah@yahoo.com>
2024-11-08 10:56:26 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ItemFormComponent } from './components/item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemListComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule,
